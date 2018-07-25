c1d5f2e378a7d0ed4c6fd468264ffebdd2c67e60
yuxiya <yuxiya@xiaomi.com>
2018-07-25 17:49:05 +0800

    update string.go

--- string.go
+++ string.go
@@ -7,5 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("%q\n", strings.SplitAfterN("a,b,c", "/n", 5))
+	fmt.Printf("%q\n", strings.SplitAfterN("a,b,c", "/n", 2))
+	fmt.Printf(strings.ToLower("ABCDEFGHIJKLMN"))
+
 }
