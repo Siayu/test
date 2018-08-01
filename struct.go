c322b5ac5478d3fe76e47b8690896a936ff46de5
yuxiya <yuxiya@xiaomi.com>
2018-08-01 09:42:17 +0800

    update struct.go

--- struct.go
+++ struct.go
@@ -13,10 +13,6 @@ type User struct{
 	 Age int
 }
 
-type Userslice struct {
-	Users []User
-}
-
 func main(){
 	http.HandleFunc("/test/jsonencode", handler1)
 	http.HandleFunc("/test/jsondecode", handler2)
@@ -24,51 +20,40 @@ func main(){
 }
 
 func handler1 (w http.ResponseWriter, r *http.Request) {
-	var name [2] User
-	name[0] = User {"Jane", 20}
-	name[1] = User {"Bob", 20}
-	//var u Userslice
-	//u.Users = append(u.Users, User{Name: "Fred", Age: 25})
-	//u.Users = append(u.Users, User{Name: "Ann", Age: 40})
-	//u.Users = append(u.Users, User{Name: "Sean", Age: 50})
-	k:=0
-	 for k<=1{
-	 f, err := json.Marshal(name[k])
+	var u [3] User
+	u[0] = User{Name: "Fred", Age: 25}
+	u[1] = User{Name: "Ann", Age: 40}
+	u[2] = User{Name: "Sean", Age: 50}
+	 f, err := json.Marshal(u)
 		if err != nil {
 			fmt.Println("json err:", err)
 		}
-		k++
 		fmt.Println(string(f))
 		fmt.Fprintln(w, string(f))
-	}
-	//b, err :=json.Marshal(u)
-	//if(err!=nil){
-	//	fmt.Println("json err:",err)
-	//}
-	//fmt.Fprintln(w,string(b))
 }
 
 
+
 func handler2 (w http.ResponseWriter, r *http.Request) {
-	var u interface{}
+	var u [] User
 	resp, err := http.Get("http://localhost:9999/test/jsonencode")
 	if err != nil {
-		//fmt.Println(err)
+		fmt.Println(err)
 	} else {
 		defer resp.Body.Close()
 		content, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal("%s", err)
 		}
-		//fmt.Println(content)
-		//fmt.Fprintln(w, string(content))
-		//fmt.Println(string(content))
-		k := 0
-		for k <= 1 {
-			str := content
-			json.Unmarshal([]byte(str), &u)
-			//fmt.Println(u)
-			//fmt.Fprintln(w, u)
+		str:= string(content)
+		fmt.Fprintln(w,str)
+		//fmt.Fprintln(w, content)
+		fmt.Fprintln(w, content)
+		json.Unmarshal([]byte(str), &u)
+		fmt.Println(u[1])
+		k :=0
+		for k<=2 {
+			fmt.Fprintln(w, u[k])
 			k++
 		}
 	}
