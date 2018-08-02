695cfd732476e4afa8a246777c733c54040e3a67
yuxiya <yuxiya@xiaomi.com>
2018-08-02 17:23:58 +0800

    update practice.go

--- practice.go
+++ practice.go
@@ -13,79 +13,107 @@ type response struct{
 	Msg string
 	Data string
 }
+type users struct{
+	Uid string
+	Name string
+	Age string
+}
 
 func main() {
 	http.HandleFunc("/submituser", submituser)
 	http.HandleFunc("/queryuser", queryuser)
 
-	log.Fatal(http.ListenAndServe(":8484", nil))
+	log.Fatal(http.ListenAndServe(":8404", nil))
 }
 //add do method to get data (HMGET)
 func submituser(w http.ResponseWriter, r *http.Request){
-	//fmt.Fprintf(w,"1")
 	var a response
 	c, err := redis.Dial("tcp", ":6379")
 	if err != nil {
-	fmt.Println(err)
+		fmt.Println("error is" ,err)
 	}
-	exists ,err := redis.Bool(c.Do("HGET","submituser","1"))
+	var x,y,z string
+	fmt.Println("Input the uid, name and age")
+	fmt.Scanf("%s",&x)
+	c.Do("HSET", "submituser", "uid"+x, x )
+	fmt.Scanf("%s",&y)
+	c.Do("HSET", "submituser", "name"+x, y)
+	fmt.Scanf("%s",&z)
+	c.Do("HSET", "submituser", "age"+x, z)
+	c.Do("HGET", "submituser", "uid"+x)
+	c.Do("HGET", "submituser", "name"+x)
+	c.Do("HGET", "submituser", "age"+x)
+	exists ,err := redis.Bool(c.Do("HEXISTS","submituser","uid"+x))
 	if err != nil{
 		fmt.Println("error", err)
 	}
-	//c.Send("HMGET", "submituser","")
-	//c.Flush()
-	//c.Receive() // reply from SET
-	//v, err = c.Receive() // reply from GET
 	if exists {
 		a.Code = 0
 		a.Msg = "ok"
 		a.Data = "{}"
 	}else{
-			a.Code= 1001
-			a.Msg= "错误的原因"
-			a.Data = "{}"
-			}
+		a.Code= 1001
+		a.Msg= "错误的原因"
+		a.Data = "{}"
+	}
 	m,err:=json.Marshal(a)
 	if err!=nil{
 		fmt.Println(err)
 	}
+	fmt.Println(string(m))
 	fmt.Fprintf(w,string(m))
 	}
 
 
 func queryuser (w http.ResponseWriter, r *http.Request){
-	//fmt.Fprintf(w, "1")
-	var a response
-	c, err := redis.Dial("tcp", ":6379")
-	if err != nil {
-		fmt.Println(err)
-	}
-	exists ,err := redis.Bool(c.Do("HEXISTS","submituser","1"))
-	if err != nil{
-		fmt.Println(err)
-	}
-
-	if exists {
-		c.Send("HGET", "submituser","1")
-		c.Flush()
-		v, err := c.Receive() // reply from GET
-		if err != nil{
-			fmt.Println(err)
+		var a response
+		var b users
+		c, err := redis.Dial("tcp", ":6379")
+		if err != nil {
+			fmt.Println("error is" ,err)
 		}
 		var x string
-		a.Code = 0
-		a.Msg = "ok"
-		a.Data= fmt.Sprintf(x,v)
-	}else{
-		a.Code= 1001
-		a.Msg= "错误的原因"
-		a.Data= "{}"
-	}
-	m,err:=json.Marshal(a)
-	if err!=nil{
-		fmt.Println(err)
-	}
+		fmt.Println("Input the uid")
+		fmt.Scanf("%s", &x)
+		exists ,err := redis.Bool(c.Do("HEXISTS","submituser","uid"+x))
+		if err != nil{
+			fmt.Println("error is ",err)
+		}
+		if exists {
+			c.Do("HGET", "submituser", "uid"+x)
+			c.Do("HGET","submituser", "name"+x)
+			c.Do("HGET", "submituser", "age"+x)
+			v1, err :=redis.String(c.Do("HGET", "submituser", "uid"+x))
+			if err != nil {
+				fmt.Println(err)
+			}
+			v2, err :=redis.String(c.Do("HGET","submituser", "name"+x))
+			if err != nil {
+				fmt.Println(err)
+			}
+			v3, err :=redis.String(c.Do("HGET", "submituser", "age"+x))
+			fmt.Println(v1)
+			if err != nil {
+				fmt.Println(err)
+			}
+			b.Uid = v1
+			b.Name = v2
+			b.Age = v3
+			q,err:=json.Marshal(b)
+			a.Code = 0
+			a.Msg = "ok"
+			a.Data = string(q)
+		}else{
+			a.Code= 1001
+			a.Msg= "错误的原因"
+			a.Data= "{}"
+		}
+		m,err:=json.Marshal(a)
+		if err!=nil{
+			fmt.Println(err)
+		}
 
-	fmt.Fprintln(w,string(m))
+		fmt.Println(string(m))
+		fmt.Fprintln(w,string(m))
 
 }
