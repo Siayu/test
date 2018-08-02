a22b21c1ed1c8f56fe87e45a1251dbeb09dd1270
yuxiya <yuxiya@xiaomi.com>
2018-08-02 17:40:57 +0800

    update practice.go

--- practice.go
+++ practice.go
@@ -66,54 +66,54 @@ func submituser(w http.ResponseWriter, r *http.Request){
 
 
 func queryuser (w http.ResponseWriter, r *http.Request){
-		var a response
-		var b users
-		c, err := redis.Dial("tcp", ":6379")
+	var a response
+	var b users
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		fmt.Println("error is" ,err)
+	}
+	var x string
+	fmt.Println("Input the uid")
+	fmt.Scanf("%s", &x)
+	exists ,err := redis.Bool(c.Do("HEXISTS","submituser","uid"+x))
+	if err != nil{
+		fmt.Println("error is ",err)
+	}
+	if exists {
+		c.Do("HGET", "submituser", "uid"+x)
+		c.Do("HGET","submituser", "name"+x)
+		c.Do("HGET", "submituser", "age"+x)
+		v1, err :=redis.String(c.Do("HGET", "submituser", "uid"+x))
 		if err != nil {
-			fmt.Println("error is" ,err)
-		}
-		var x string
-		fmt.Println("Input the uid")
-		fmt.Scanf("%s", &x)
-		exists ,err := redis.Bool(c.Do("HEXISTS","submituser","uid"+x))
-		if err != nil{
-			fmt.Println("error is ",err)
-		}
-		if exists {
-			c.Do("HGET", "submituser", "uid"+x)
-			c.Do("HGET","submituser", "name"+x)
-			c.Do("HGET", "submituser", "age"+x)
-			v1, err :=redis.String(c.Do("HGET", "submituser", "uid"+x))
-			if err != nil {
-				fmt.Println(err)
-			}
-			v2, err :=redis.String(c.Do("HGET","submituser", "name"+x))
-			if err != nil {
-				fmt.Println(err)
-			}
-			v3, err :=redis.String(c.Do("HGET", "submituser", "age"+x))
-			fmt.Println(v1)
-			if err != nil {
-				fmt.Println(err)
-			}
-			b.Uid = v1
-			b.Name = v2
-			b.Age = v3
-			q,err:=json.Marshal(b)
-			a.Code = 0
-			a.Msg = "ok"
-			a.Data = string(q)
-		}else{
-			a.Code= 1001
-			a.Msg= "错误的原因"
-			a.Data= "{}"
-		}
-		m,err:=json.Marshal(a)
-		if err!=nil{
 			fmt.Println(err)
 		}
+		v2, err :=redis.String(c.Do("HGET","submituser", "name"+x))
+		if err != nil {
+			fmt.Println(err)
+		}
+		v3, err :=redis.String(c.Do("HGET", "submituser", "age"+x))
+		fmt.Println(v1)
+		if err != nil {
+			fmt.Println(err)
+		}
+		b.Uid = v1
+		b.Name = v2
+		b.Age = v3
+		q,err:=json.Marshal(b)
+		a.Code = 0
+		a.Msg = "ok"
+		a.Data = string(q)
+	}else{
+		a.Code= 1001
+		a.Msg= "错误的原因"
+		a.Data= "{}"
+		}
+	m,err:=json.Marshal(a)
+	if err!=nil{
+		fmt.Println(err)
+	}
 
-		fmt.Println(string(m))
-		fmt.Fprintln(w,string(m))
+	fmt.Println(string(m))
+	fmt.Fprintln(w,string(m))
 
 }
