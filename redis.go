ebbbd52a152ad6173292623b132fee5cc2c1bb2a
yuxiya <yuxiya@xiaomi.com>
2018-08-02 17:25:11 +0800

    update redis.go

--- redis.go
+++ redis.go
@@ -3,53 +3,67 @@ package main
 import (
 	"github.com/gomodule/redigo/redis"
 	"fmt"
+	"encoding/json"
 )
 
 type response1 struct{
-	code int
-	msg string
-	data string
+	Code int
+	Msg string
+	Data string
 }
-
+type users1 struct {
+	response1
+	Uid  string
+	Name string
+	Age  string
+}
 func main() {
-
 	var a response1
-	//resp, err := http.Get("http://localhost:6379/submituser")
-	//defer resp.Body.Close()
+	var b users
 	c, err := redis.Dial("tcp", ":6379")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error is" ,err)
 	}
-	exists ,err := redis.Bool(c.Do("EXISTS","ann"))
+	var x string
+	fmt.Println("Input the uid")
+	fmt.Scanf("%s", &x)
+	exists ,err := redis.Bool(c.Do("HEXISTS","submituser","uid"+x))
 	if err != nil{
-		fmt.Println(err)
+		fmt.Println("error is ",err)
 	}
 	if exists {
-		a.code = 0
-		a.msg = "ok"
+		c.Do("HGET", "submituser", "uid"+x)
+		c.Do("HGET","submituser", "name"+x)
+		c.Do("HGET", "submituser", "age"+x)
+		v1, err :=redis.String(c.Do("HGET", "submituser", "uid"+x))
+		if err != nil {
+			fmt.Println(err)
+		}
+		v2, err :=redis.String(c.Do("HGET","submituser", "name"+x))
+		if err != nil {
+			fmt.Println(err)
+		}
+		v3, err :=redis.String(c.Do("HGET", "submituser", "age"+x))
+		fmt.Println(v1)
+		if err != nil {
+			fmt.Println(err)
+		}
+		b.Uid = v1
+		b.Name = v2
+		b.Age = v3
+		q,err:=json.Marshal(b)
+		a.Code = 0
+		a.Msg = "ok"
+		a.Data = string(q)
 	}else{
-		a.code= 1001
-		a.msg= "错误的原因"
+		a.Code= 1001
+		a.Msg= "错误的原因"
+		a.Data= "{}"
 	}
-
-
-	var b response1
-	//resp, err := http.Get("http://localhost:6379/userquery")
-	//defer resp.Body.Close()
-	c, err := redis.Dial("tcp", ":6379")
-	if err != nil {
+	m,err:=json.Marshal(a)
+	if err!=nil{
 		fmt.Println(err)
 	}
-	exists ,err := redis.Bool(c.Do("EXISTS","uid"))
-	if err != nil{
-		fmt.Println(err)
-	}
 
-	if exists {
-		a.code = 0
-		a.msg = "ok"
-	}else{
-		a.code= 1001
-		a.msg= "错误的原因"
-	}
+	fmt.Println(string(m))
 }
